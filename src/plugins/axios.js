da2d219374f81018c90b26c5902a955ac09d60e2
Dave <ddare@unifiedcompliance.com>
2020-11-20 11:06:26 -0800

    Fix issue with env variables not loading

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const customHeaders = {
-  'x-rapidapi-host': process.env.APP_API_HOST,
-  'x-rapidapi-key': process.env.APP_API_KEY,
-  useQueryString: process.env.APP_API_USE_QUERY_STRING
+  'x-rapidapi-host': process.env.VUE_APP_API_HOST,
+  'x-rapidapi-key': process.env.VUE_APP_API_KEY,
+  useQueryString: process.env.VUE_APP_API_USE_QUERY_STRING
 };
 
 const instance = axios.create({
-  baseURL: process.env.APP_API_BASE_URL,
+  baseURL: process.env.VUE_APP_API_BASE_URL,
   headers: customHeaders
 });
 
